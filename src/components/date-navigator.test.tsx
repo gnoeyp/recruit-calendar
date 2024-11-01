f48a2e83bc3b2f7440e14dc29279676cdc441c00
gnoeyp <ephong93@gmail.com>
2024-11-01 23:32:22 +0900

    feat: date-navigator 구현

---
+++ src/components/date-navigator.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
+import DateNavigator from './date-navigator';
+import { YearMonth } from '@/utils/year-month';
+
+describe('DateNavigator', () => {
+  it('현재 연월을 표시해야 한다.', () => {
+    const yearMonth = new YearMonth(2024, 10);
+    render(<DateNavigator yearMonth={yearMonth} />);
+
+    expect(screen.getByText('2024 - 10')).toBeDefined();
+  });
+
+  it('previous month 버튼이 눌렸을 때 onChange가 호출되어야 한다.', () => {
+    const yearMonth = new YearMonth(2024, 10);
+    const handleChange = vi.fn();
+    render(<DateNavigator yearMonth={yearMonth} onChange={handleChange} />);
+    fireEvent.click(screen.getByRole('button', { name: /previous-month/i }));
+    expect(handleChange).toHaveBeenCalledWith(yearMonth.previousMonth());
+  });
+
+  it('next month 버튼이 눌렸을 때 onChange가 호출되어야 한다.', () => {
+    const yearMonth = new YearMonth(2024, 10);
+    const handleChange = vi.fn();
+    render(<DateNavigator yearMonth={yearMonth} onChange={handleChange} />);
+
+    fireEvent.click(screen.getByRole('button', { name: /next-month/i }));
+    expect(handleChange).toHaveBeenCalledWith(yearMonth.nextMonth());
+  });
+});
--- src/components/date-navigator.tsx
+++ src/components/date-navigator.tsx
@@ -1,3 +1,30 @@
-export default function DateNavigator() {
-  return <div></div>;
+import { YearMonth } from '@/utils/year-month';
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa6';
+
+type DateNavigatorProps = {
+  yearMonth: YearMonth;
+  onChange?: (yearMonth: YearMonth) => void;
+};
+
+export default function DateNavigator({
+  yearMonth,
+  onChange,
+}: DateNavigatorProps) {
+  return (
+    <div className="flex items-center gap-2 text-lg">
+      <button
+        onClick={() => onChange?.(yearMonth.previousMonth())}
+        aria-label="previous-month"
+      >
+        <FaAngleLeft />
+      </button>
+      {yearMonth.year} - {yearMonth.month}
+      <button
+        onClick={() => onChange?.(yearMonth.nextMonth())}
+        aria-label="next-month"
+      >
+        <FaAngleRight />
+      </button>
+    </div>
+  );
 }
