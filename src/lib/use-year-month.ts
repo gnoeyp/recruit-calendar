b7a897760a07d5f665d313101c4f03f9521fd031
gnoeyp <ephong93@gmail.com>
2024-11-01 22:43:46 +0900

    refactor: useYearMonth deps

--- src/lib/use-year-month.ts
+++ src/lib/use-year-month.ts
@@ -2,24 +2,49 @@
 
 import { YearMonth } from '@/utils/year-month';
 import { useParams, useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function useYearMonth() {
   const { yearmonth: yearMonthStr } = useParams<{ yearmonth: string }>();
   const [yearMonth, setYearMonth] = useState<YearMonth | null>();
   const router = useRouter();
 
+  const goTo = useCallback(
+    (targetYearMonth: YearMonth) => {
+      router.push(
+        `/${targetYearMonth.year}-${String(targetYearMonth.month).padStart(
+          2,
+          '0',
+        )}`,
+      );
+    },
+    [router],
+  );
+
+  const goToCurrent = useCallback(() => {
+    const currentYearMonth = new YearMonth(
+      new Date().getFullYear(),
+      new Date().getMonth() + 1,
+    );
+    goTo(currentYearMonth);
+  }, [goTo]);
+
   useEffect(() => {
-    const tokens = yearMonthStr.split('-').map(Number);
-    if (tokens.length !== 2) {
-      throw new Error();
+    try {
+      const tokens = yearMonthStr.split('-').map(Number);
+      if (tokens.length !== 2) {
+        throw new Error();
+      }
+      const [year, month] = tokens;
+
+      setYearMonth(new YearMonth(year, month));
+    } catch {
+      goToCurrent();
     }
-    const [year, month] = tokens;
-    setYearMonth(new YearMonth(month, year));
-  }, [yearMonthStr]);
+  }, [yearMonthStr, goToCurrent]);
 
   const handleChange = (newYearMonth: YearMonth) => {
-    router.push(`/${newYearMonth.toString()}`);
+    goTo(newYearMonth);
   };
 
   return {
