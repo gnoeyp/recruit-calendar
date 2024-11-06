2007fc76813c3520da6baabfaea156199d540be4
gnoeyp <ephong93@gmail.com>
2024-11-06 20:37:39 +0900

    feat: add onClick callback

--- src/components/job-opening-display.tsx
+++ src/components/job-opening-display.tsx
@@ -4,14 +4,19 @@ import TinyBadge from './ui/tiny-badge';
 type JobOpeningDisplayProps = {
   jobOpening: JobOpening;
   status?: 'starting' | 'ending';
+  onClick?: () => void;
 };
 
 export default function JobOpeningDisplay({
   jobOpening,
   status,
+  onClick,
 }: JobOpeningDisplayProps) {
   return (
-    <div className="flex items-center gap-1 text-sm bg-white hover:bg-slate-100 cursor-pointer select-none px-2 py-0.5 w-full">
+    <div
+      className="flex items-center gap-1 text-sm bg-white hover:bg-slate-100 cursor-pointer select-none px-2 py-0.5 w-full"
+      onClick={onClick}
+    >
       {status === 'starting' && <TinyBadge type="light">시</TinyBadge>}
       {status === 'ending' && <TinyBadge type="dark">끝</TinyBadge>}
       <div className="flex-1 overflow-hidden text-nowrap">
