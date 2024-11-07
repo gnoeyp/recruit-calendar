4feca9e036786a7c33239092798fc6a4a2b67533
gnoeyp <ephong93@gmail.com>
2024-11-07 19:57:14 +0900

    fix: initialize visitedIds in useEffect

--- src/hooks/use-visited.ts
+++ src/hooks/use-visited.ts
@@ -11,16 +11,17 @@ type UseVisitedReturn = [string[], (visited: string[]) => void];
  * 방문한 ID 목록과 목록을 업데이트하는 함수를 포함하는 배열을 반환합니다.
  */
 export default function useVisited(): UseVisitedReturn {
-  const [visitedIds, setVisitedIds] = useState<string[]>(() => {
-    if (typeof window === 'undefined') return [];
-    const storedVisited = localStorage.getItem(VISITED_KEY);
-    return storedVisited ? JSON.parse(storedVisited) : [];
-  });
+  const [visitedIds, setVisitedIds] = useState<string[]>([]);
 
+  const setVisitedIdsWrapper = (visited: string[]) => {
+    localStorage.setItem(VISITED_KEY, JSON.stringify(visited));
+    setVisitedIds(visited);
+  };
+
+  // SSR에서는 localStorage를 사용할 수 없으므로 useEffect를 사용하여 초기화합니다.
   useEffect(() => {
-    if (typeof window === 'undefined') return;
-    localStorage.setItem(VISITED_KEY, JSON.stringify(visitedIds));
-  }, [visitedIds]);
+    setVisitedIds(JSON.parse(localStorage.getItem(VISITED_KEY) ?? '[]'));
+  }, []);
 
-  return [visitedIds, setVisitedIds];
+  return [visitedIds, setVisitedIdsWrapper];
 }
