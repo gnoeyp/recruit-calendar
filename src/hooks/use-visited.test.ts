80c39fc8e18073aa130c6d727b81ee7e30e57f97
gnoeyp <ephong93@gmail.com>
2024-11-08 07:57:46 +0900

    tests: fix test error

--- src/hooks/use-visited.test.ts
+++ src/hooks/use-visited.test.ts
@@ -2,26 +2,28 @@ import { renderHook, act } from '@testing-library/react';
 import useVisited from './use-visited';
 import { beforeEach, describe, expect, it } from 'vitest';
 
+const LOCALSTORAGE_KEY = 'visited';
+
 describe('useVisited', () => {
   beforeEach(() => {
     localStorage.clear();
   });
 
   it('localStorage가 비어있으면 빈 배열로 초기화되어야 한다', () => {
-    const { result } = renderHook(() => useVisited());
+    const { result } = renderHook(() => useVisited(LOCALSTORAGE_KEY));
     const [visitedIds] = result.current;
     expect(visitedIds).toEqual([]);
   });
 
   it('localStorage에 값이 있으면 해당 값으로 초기화되어야 한다', () => {
     localStorage.setItem('visited', JSON.stringify(['id1', 'id2']));
-    const { result } = renderHook(() => useVisited());
+    const { result } = renderHook(() => useVisited(LOCALSTORAGE_KEY));
     const [visitedIds] = result.current;
     expect(visitedIds).toEqual(['id1', 'id2']);
   });
 
   it('visitedIds가 변경되면 localStorage가 업데이트되어야 한다', () => {
-    const { result } = renderHook(() => useVisited());
+    const { result } = renderHook(() => useVisited(LOCALSTORAGE_KEY));
     const [, setVisitedIds] = result.current;
 
     act(() => {
@@ -34,7 +36,7 @@ describe('useVisited', () => {
   });
 
   it('visitedIds 상태가 업데이트되어야 한다', () => {
-    const { result } = renderHook(() => useVisited());
+    const { result } = renderHook(() => useVisited(LOCALSTORAGE_KEY));
     const [, setVisitedIds] = result.current;
 
     act(() => {
